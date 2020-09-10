af22e461dc5c9d4fd8eccfcdd84a055d5feacace
dominoa <1600635053@qq.com>
2020-09-10 11:00:41 +0800

    添加了Vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    111
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './style/icon.less'
+import Vant from 'vant' // Vant组件
+import 'vant/lib/index.css' // Vant样式
+
+// 注册使用Vant组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
